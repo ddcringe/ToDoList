60d4a6bd18690a34b6913e0597e0b552110040ff
Пользователь <@DESKTOP-OLKK79S>
2023-07-30 01:59:03 +0300

    Add project files.

---
+++ ToDoList.cpp
@@ -0,0 +1,115 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<cstdio>
+#include<unordered_map>
+#include<unordered_set>
+#include<time.h>
+
+using namespace std;
+
+class task
+{
+private:
+	string name, description;
+	int priority, id;
+public:
+	bool set_name(string Name) {
+		if (Name.size() > 20) {
+			printf("--Invalid name\n --Please make it less than 20 symbols");
+			return false;
+		}
+		else {
+			name = Name;
+			return true;
+		}
+	}
+	bool set_priority(int Pri) {
+		if (Pri < 0 || Pri>99) {
+			printf("--Invalid priority\n --Please make it from 0 to 99");
+			return false;
+		}
+		else {
+			priority = Pri;
+			return true;
+		}
+	}
+	void set_description(string Des) {
+		description = Des;
+	}
+	void set_category(string Cat) {
+
+	}
+	void set_time() {
+
+	}
+	void set_id(int Id) {
+		//id=
+	}
+	void print() {
+		
+	}
+	void show_description() {
+
+	}
+};
+
+
+class list_of_tasks
+{
+private:
+	vector<task> li;
+	unordered_set<string> categories;
+
+public:
+	void add_task() {
+
+	}
+	void remove_task() {
+
+	}
+	void filter_tasks() {
+
+	}
+	void sort_tasks() {
+
+	}
+	void remove_all() {
+
+	}
+	void change_task() {
+
+	}
+	void add_category() {
+
+	}
+	void remove_category() {
+
+	}
+	void show() {
+		printf("%4s  %20s  %2s", "id", "name       ", "pr");
+		for (task t : li) {
+			
+		}
+	}
+	int get_number_of_tasks() {
+		return li.size();
+	}
+
+};
+
+int main() {
+	list_of_tasks li;
+	printf("Please enter a command\n");
+	printf(" -add- to add a task \n");
+	printf(" -remove- -id- to remove a task with id\n");
+	printf(" -show- to show all curent tasks \n");
+	printf(" -change- -id- to change in task with id \n");
+	printf(" -filter- -id- to change in task with id \n");
+	printf(" -filter- -id- to change in task with id \n");
+	printf(" -remall- to remove all current tasks \n");
+	time_t raw;
+	time(&raw);
+	printf("cur time %s", ctime(&raw));
+
+}
